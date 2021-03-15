b68add57cefc8bca947abeef23e2db3a467100dc
d.vinokurova <d.vinokurova@fasten.com>
2021-03-15 17:17:14 +0300

    Add precondition files for mantis tests

---
+++ mantis/src/test/java/tests/TestBase.java
@@ -0,0 +1,24 @@
+package tests;
+
+import appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME)); //if browser type is not specified use chrome
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite (alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
